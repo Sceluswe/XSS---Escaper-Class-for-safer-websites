55413a8ebb3e374c1def549a5a346899fce69517
Emil Mattsson <emilcbm@gmail.com>
2016-04-06 18:29:16 +0200

    Final stable version
    Includes URL espacing, XML and XML attribute escpaing functions.

--- CEscaper.php
+++ CEscaper.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+* All functions are based on the recommendations in the 
+* XSS (Cross Site Scripting) Prevention Cheat Sheet:
+* https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet
+* 
+*/
+
 class Escaper
 {
 	private $CHARSET;
@@ -12,12 +20,23 @@ class Escaper
 		$this->CHARSET = $_SESSION['escaper_charset'];
 	}
 	
+	/** Sets the used charset for the esacpeHTML and escapeXML function.
+	*
+	* @param $string, the string to escape.
+	*
+	*/
 	public function setCharset($string)
 	{
 		$_SESSION['escaper_charset'] = strip_tags($string);
 		$this->CHARSET = $_SESSION['escaper_charset'];
 	}
 	
+	/** Escapes HTML string using htmlspecialchars().
+	*
+	* @param $string, the untrusted string to escape.
+	*
+	* @return $result, escaped string.
+	*/
 	public function escapeHTML($string)
 	{
 		$result = htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE, $this->CHARSET);
@@ -26,6 +45,12 @@ class Escaper
 		return $result;
 	}
 	
+	/** Escapes non-alphanumeric characters in an untrusted string for HTML attribute values.
+	*
+	* @param $string, the untrusted string to escape.
+	*
+	* @return $result, escaped string.
+	*/
 	public function escapeHTMLattr($string)
 	{
 		$result = preg_replace_callback("/[\W]/", function ($matches){
@@ -36,6 +61,12 @@ class Escaper
 		return $result;
 	}
 	
+	/** Escapes non-alphanumeric characters in an untrusted string for JS input values.
+	*
+	* @param $string, the untrusted string to escape.
+	*
+	* @return $result, escaped string.
+	*/
 	public function escapeJs($string)
 	{
 		$result = preg_replace_callback("/[\W]/", function ($matches){
@@ -46,6 +77,12 @@ class Escaper
 		return $result;
 	}
 	
+	/** Escapes non-alphanumeric characters in an untrusted string for CSS input values.
+	*
+	* @param $string, the untrusted string to escape.
+	*
+	* @return $result, escaped string.
+	*/
 	public function escapeCSS($string)
 	{
 		$result = preg_replace_callback("/[\W]/", function ($matches){
@@ -54,5 +91,30 @@ class Escaper
 		$string);
 
 		return $result;
+	}
+	
+	/** Escapes data that is to be inserted in a URL not the whole URL itself.
+	* 
+	* @param $string, the untrusted string to escape.
+	*
+	* @return, escaped string.
+	*/
+	public function escapeUrl($string)
+	{
+		return rawurlencode($string);
+	}
+	
+	/**
+	* Aliases to HTML functions for semantic value.
+	* XML escaping is identical to HTML escaping.
+	*/
+	public function escapeXml($string)
+	{
+		return $this->escapeHTML($string);
+	}
+
+	public function escapeXmlAttr($string)
+	{
+		return $this->escapeHTMLattr($string);
 	}
 }
