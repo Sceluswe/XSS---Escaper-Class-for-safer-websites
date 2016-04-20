90d9d60dbf971220e807fb8668a603366477d958
Sceluswe <emilcbm@gmail.com>
2016-04-20 19:43:36 +0200

    Removed extra lines

--- test/Escaper/CEscaperTest.php
+++ test/Escaper/CEscaperTest.php
@@ -11,10 +11,10 @@ namespace Scelus\Escaper;
 class CEscaperTest extends \PHPUnit_Framework_TestCase
 {
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testConstructorSuccess() {
 		$el = new \Scelus\Escaper\CEscaper();
 
@@ -22,12 +22,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
         $exp = 'UTF-8';
         $this->assertEquals($res, $exp, "Created element name missmatch.");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	 * Test 
+	 *
+	 * @return void
+	 */
 	public function testSetAndGetEncoding() {
 		$el = new \Scelus\Escaper\CEscaper('ASCII');
 		
@@ -40,12 +40,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = 'UTF-8';
 		$this->assertEquals($res, $exp, "setEncoding() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeHTML() {
 		$el = new \Scelus\Escaper\CEscaper();
 		
@@ -54,12 +54,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = '&gt;&lt;&#x2F;div&gt;&lt;h1&gt;myattack&lt;&#x2F;h1&gt;';
 		$this->assertEquals($res, $exp, "escapeHTML() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeHTMLattr() {
 		$el = new \Scelus\Escaper\CEscaper();
 		
@@ -68,12 +68,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = "&#x22;&#x3e;&#x3c;h1&#x3e;Hello&#x3c;&#x2f;table";
 		$this->assertEquals($res, $exp, "escapeHTMLattr() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeUrl() {
 		$el = new \Scelus\Escaper\CEscaper();
 		
@@ -82,12 +82,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = "%22%3E%3Cscript%3Ealert%281%29%3C%2Fscript%3E%3Ca%20href%3D%22%23";
 		$this->assertEquals($res, $exp, "escapeUrl() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeCSS() {
 		$el = new \Scelus\Escaper\CEscaper();
 		
@@ -96,12 +96,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = '\22 \3e \3c script\3e alert\28 1\29 \3c \2f script\3e \3c a\20 href\3d \22 \23 ';
 		$this->assertEquals($res, $exp, "escapeCSS() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeJs() {
 		$el = new \Scelus\Escaper\CEscaper();
 		$argument = "'; alert(100); var x='";
@@ -109,12 +109,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = '\x27\x3b\x20alert\x28100\x29\x3b\x20var\x20x\x3d\x27';
 		$this->assertEquals($res, $exp, "escapeJs() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeXML() {
 		$el = new \Scelus\Escaper\CEscaper();
 		$argument = '></div><h1>myattack</h1>';
@@ -122,12 +122,12 @@ class CEscaperTest extends \PHPUnit_Framework_TestCase
 		$exp = '&gt;&lt;&#x2F;div&gt;&lt;h1&gt;myattack&lt;&#x2F;h1&gt;';
 		$this->assertEquals($res, $exp, "escapeHTML() produced missmatch");
 	}
-	
+
 	/**
-     * Test 
-     *
-     * @return void
-     */
+	* Test 
+	*
+	* @return void
+	*/
 	public function testEscapeXmlattr() {
 		$el = new \Scelus\Escaper\CEscaper();
 		$argument = '"><h1>Hello</table';
